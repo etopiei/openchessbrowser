5b2e59da3c74cc800359f1e7460c05e3887376a7
etopiei <lj343@icloud.com>
2019-05-23 19:38:29 +1000

    Worked on basic functionality for webapp

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use chess::{Board, ChessMove};
+use pleco::{Board, BitMove, MoveList};
+use pleco::core::{File, Rank};
+use pleco::SQ;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -12,13 +14,8 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub struct ChessBrowser {
     current_board: Board,
-    new_board: Board,
-    moves: [ChessMove; 256],
-    moveHistory: Vec<ChessMove>
-}
-
-fn createMoveFromString(moveString: &str) -> ChessMove {
-    return ChessMove::default();
+    moves: MoveList,
+    moveHistory: Vec<BitMove>,
 }
 
 #[wasm_bindgen]
@@ -26,17 +23,15 @@ impl ChessBrowser {
 
     pub fn new () -> ChessBrowser {
         // Create a global board and moves object for game state
-        let current_board = Board::default();
-        let new_board = Board::default();
-        let moves = [ChessMove::default(); 256];
-        let moveHistory: Vec<ChessMove> = Vec::new();
+        let current_board = Board::start_pos();
+        let moves = MoveList::default();
+        let moveHistory = Vec::new();
 
         // Initalise object with state and return it
         ChessBrowser {
             current_board,
-            new_board,
             moves,
-            moveHistory
+            moveHistory,
         }
     }
 
@@ -48,22 +43,46 @@ impl ChessBrowser {
         moves_string
     }
 
-    pub fn makeMove(&mut self, moveString: &str) -> String {
-        // Create the move
-        let newMove = createMoveFromString(moveString);
-        // Make move on the board
-        self.current_board.make_move(newMove, &mut self.new_board);
-        self.current_board = self.new_board;
-        // Add move to move history
+    fn addToHistory(&mut self, newMove: BitMove) {
         self.moveHistory.push(newMove);
+    }
+
+    fn playMove(&mut self, newMove: &BitMove) {
+        self.current_board.apply_move(*newMove);
+    }
+
+    fn makeBitMoveFromString(moveString: &str) -> BitMove {
+        let srcFile = File::A;
+        let srcRank = Rank::R2;
+        let srcSquare = SQ::make(srcFile, srcRank);
+
+        let destFile = File::A;
+        let destRank = Rank::R3;
+        let destSquare = SQ::make(destFile, destRank);
+
+        BitMove::make(0, srcSquare, destSquare)
+    }
+
+    pub fn makeMove(&mut self, moveString: &str) -> String {
+        // Create the move from string
+        let newMove: BitMove = ChessBrowser::makeBitMoveFromString(moveString);
+        // Make move on the board
+        self.playMove(&newMove);
+        // Add move to move history
+        self.addToHistory(newMove);
         // Update moves
-        self.current_board.enumerate_moves(&mut self.moves);
+        self.moves = self.current_board.generate_moves();
+        // Return the moves
         self.movesToStr()
     }
 
     pub fn getInitalMoves(&mut self) -> String {
         // Update moves to represent all possible moves from the current board state
-        self.current_board.enumerate_moves(&mut self.moves);
+        self.moves = self.current_board.generate_moves();
         self.movesToStr()
+    }
+
+    pub fn getFEN(self) -> String {
+        self.current_board.fen()
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,13 +1,36 @@
-import { ChessBrowser } from "openchessbrowser";
+const wasm_chess = require('../pkg/openchessbrowser');
+const ChessBrowser = wasm_chess.ChessBrowser;
 
 let moves = [];
+let fen = '';
 let chessbrowser = ChessBrowser.new();
 
+const dropPiece = (src, dest) => {
+    // check move valid
+    const valid = moves.includes(src + dest) ? true : 'snapback';
+    if (valid != 'snapback') {
+        // make the move
+        updateAvailableMoves(chessbrowser.makeMove(src+dest));
+        fen = chessbrowser.getFEN();
+    }
+    return valid;
+};
+
+const boardConfig = {
+    draggable: true,
+    dropOffBoard: 'snapback',
+    onDrop: dropPiece,
+    position: 'start'
+};
+
+const board = ChessBoard('chess-board', boardConfig);
+
 function updateAvailableMoves(s) {
+    moves = [];
     moves = s.split(",");
     moves.pop(); // account for superfluous comma at end of string
 }
 
-console.log(moves);
+// Initial Setup
 updateAvailableMoves(chessbrowser.getInitalMoves());
-console.log(moves);
+fen = chessbrowser.getFEN();
